7f6d0d0b1e12e96f444dc95d4c5880da9e651a56
Rene Araujo <rene29alexander@gmail.com>
2021-08-20 12:00:45 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_Ejemplo/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Ejemplo
+{
+    public class Class1
+    {
+        public static void ListCollection()
+        {
+            Console.WriteLine("ingresa el numero del dia de la semana que desee imprimir: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero == 1)
+            {
+                Console.WriteLine("Usted imprimio el dia Lunes.");
+            }
+
+            else if (numero == 2)
+            {
+                Console.WriteLine("Usted imprimio el dia Martes.");
+            }
+
+            else if (numero == 3)
+            {
+                Console.WriteLine("Usted imprimio el dia Miercoles.");
+            }
+
+            else if (numero == 4)
+            {
+                Console.WriteLine("Usted imprimio el dia Jueves.");
+            }
+
+            else if (numero == 5)
+            {
+                Console.WriteLine("Usted imprimio el dia Viernes.");
+            }
+
+            else if (numero == 6)
+            {
+                Console.WriteLine("Usted imprimio el dia Sabado.");
+            }
+
+            else if (numero == 7)
+            {
+                Console.WriteLine("Usted imprimio el dia Domingo.");
+            }
+
+            else
+            {
+                Console.WriteLine("El numero que ingreso no corresponde a un dia de la semana.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Uso_de_libreria/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_Ejemplo;
+
+namespace Uso_de_libreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.ListCollection();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
